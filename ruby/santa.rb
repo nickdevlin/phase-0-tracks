56aa949ad6709189a205d2f49e795a88e99696cc
Nick Devlin <nrdevlin@gmail.com>
2016-10-20 11:20:05 -0500

    Add Release 0.

---
+++ ruby/santa.rb
@@ -0,0 +1,12 @@
+class Santa
+	def initialize
+		"Initializing Santa instance..."
+
+	def speak
+		"Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		"That was a good #{cookie_type}!"
+	end
+end
