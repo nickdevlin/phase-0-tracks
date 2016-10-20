c58538d67fd2e5d964fed31993e7b4adf9686437
Nick Devlin <nrdevlin@gmail.com>
2016-10-20 12:21:44 -0500

    Complete Release 1.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,11 @@
 class Santa
-	def initialize
-		"Initializing Santa instance..."
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender 			= gender
+		@ethnicity			= ethnicity
+		#@reindeer_ranking	= ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age				= 0
+	end
 
 	def speak
 		"Ho, ho, ho! Haaaappy holidays!"
@@ -10,3 +15,20 @@ class Santa
 		"That was a good #{cookie_type}!"
 	end
 end
+
+kringle = Santa.new
+
+p kringle.speak
+
+p kringle.eat_milk_and_cookies("oatmeal raisin")
+
+all_santas = []
+
+possible_genders = ["female", "male", "genderfluid", "agender"]
+possible_ethnicities = ["black", "Chinese", "Maldovan", "Argentine"]
+
+possible_genders.length.times do |i|
+	all_santas << Santa.new(possible_genders[i], possible_ethnicities[i])
+end
+
+p all_santas
