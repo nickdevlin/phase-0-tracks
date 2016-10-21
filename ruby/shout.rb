44a4957acc3d96c9fc67ebd6e08448bfe2807401
Nick Devlin <nrdevlin@gmail.com>
2016-10-21 11:07:10 -0500

    Complete 6.4.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,27 @@
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell_angrily(words)
+		puts words + "!!!" + " :("
+		#removed "self." to create mixin version
 	end
 
-	def self.yell_happily(words)
-		words + " :)))))))))"
+	def yell_happily(words)
+		puts words + " :)))))))))"
 	end
 end
 
-p Shout.yell_angrily("Dag nabbit")
 
-p Shout.yell_happily("Cubs win")
+#create two classes to include Shout module
+class Cubs_fan
+	include Shout
+	# added include line to point to module
+end
+
+class Dodgers_fan
+	include Shout
+end
+
+gocubs = Cubs_fan.new
+gocubs.yell_happily("The Cubs are going to win tomorrow")
+
+boododgers = Dodgers_fan.new
+boododgers.yell_angrily("The Dodgers smell of butt and will probably lose tomorrow")
