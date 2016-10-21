a9e4e7ca029463f9c84b1aac7829f31580694ed9
Nick Devlin <nrdevlin@gmail.com>
2016-10-21 10:41:43 -0500

    Complete Release 1.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,2 +1,13 @@
 module Shout
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yell_happily(words)
+		words + " :)))))))))"
+	end
 end
+
+p Shout.yell_angrily("Dag nabbit")
+
+p Shout.yell_happily("Cubs win")
