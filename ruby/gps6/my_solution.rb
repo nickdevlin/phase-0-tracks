2b475f4df32a47a927fb471e9bbb486f3b876ab6
Nick Devlin <nrdevlin@gmail.com>
2016-10-24 19:02:09 -0500

    Take code from Jenn.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,27 +1,45 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Nick Devlin.
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Load a file that's located relative to the current file location
+# Require will check a path variable
+# require_relative is more specific and more adaptable
 require_relative 'state_data'
 
 class VirusPredictor
 
+# creates new instance of VirusPredictor class taking 3 parameters
+# setting them as instance variables
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+# returns the speed_of_spread method but runs both predicted_deaths and speed_of_spread methods
+
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
+  def self.national_report
+    STATE_DATA.each do |state, demographics|
+      nationalInstance = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
+      nationalInstance.virus_effects
+    end
+    #california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+#california.virus_effects
+  end
+
+# sets below methods to private so they can't be called upon
   private
+
+# takes parameters of population density, population, and state and sets number_of_deaths variable
+# depending on the population and population density, rounding down floats to largest integer <= float
 
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
@@ -40,6 +58,9 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
+
+  # takes in parameters of population density and state and calculates the speed
+  # in months depending on the population density
 
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
@@ -68,22 +89,18 @@ end
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
-
-
+=begin
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
-
 jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
 jersey.virus_effects
-
 california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
 california.virus_effects
-
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
+=end
 
+VirusPredictor.national_report
 
 #=======================================================================
 # Reflection Section
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
