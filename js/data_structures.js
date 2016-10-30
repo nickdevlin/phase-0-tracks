370b39b9156cb2b68fa08c90fa2d06ab838c1a16
Nick Devlin <nrdevlin@gmail.com>
2016-10-30 10:31:20 -0500

    Create arrays, build object.

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["red", "orange", "yellow", "burnt sienna"]
+var horseNames = ["Bill", "Ted", "Excellent", "Adventure"]
+
+colors.push("indigo");
+horseNames.push("Napoleon");
+
+var coloredHorses = [horseNames[0]: colors[0], horseNames[1]: colors[1], horseNames[2]: colors[2], horseNames[3]: colors[3], horseNames[4]: colors[4]]
+
