9598271f3b8410336e8d77ef97971d66243d0cbd
Nick Devlin <nrdevlin@gmail.com>
2016-11-06 19:25:11 -0600

    Tweak code from video to suit project.

---
+++ databases/concussions.rb
@@ -0,0 +1,26 @@
+require 'sqlite3'
+require 'faker'
+
+db = SQLite3::Database.new("concussions.db")
+db.results_as_hash = true
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS concussions(
+    id INTEGER PRIMARY KEY,
+    player_name VARCHAR(255),
+    year INT,
+    concussion_grade INT
+  )
+SQL
+
+db.execute(create_table_cmd)
+
+def add_concussion(db, player_name, year, concussion_grade)
+  db.execute("INSERT INTO concussions (player_name, year, concussion_grade) VALUES (?, ?, ?)", [player_name, year, concussion_grade])
+end
+
+def edit_grade 
+
+
+
+
